8e4fa55895a8dee91df808e07a7fd598ea5cdb1a
Apostrov <amir.subaev@gmail.com>
2019-01-26 10:05:36 +0300

    Add superLight skill to hero

---
+++ Tika-Ido-moi/Assets/Scripts/HeroSuperLight.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class HeroSuperLight : MonoBehaviour
+{
+    private Light _pointLight;
+    public float minLightRange = 10f;
+    public float decreasingLigntSpeed = 10f;
+    void Start()
+    {
+        _pointLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _pointLight.range = 25;
+        }
+
+        if (_pointLight.range > minLightRange)
+        {
+            _pointLight.range =
+                Mathf.SmoothStep(_pointLight.range, minLightRange, decreasingLigntSpeed * Time.deltaTime);
+        }
+        
+    }
+    
+}
