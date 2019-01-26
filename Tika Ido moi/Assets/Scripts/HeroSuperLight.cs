576bf74f78c80d6ba77a9b02ca4d29269e3d1ac1
Apostrov <amir.subaev@gmail.com>
2019-01-26 16:24:56 +0300

    Add hero super light

---
+++ Tika Ido moi/Assets/Scripts/HeroSuperLight.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class HeroSuperLight : MonoBehaviour
+{
+    public GameObject blackHole;
+    public float decreasingSpeed = 10f;
+    private Vector3 _orginScale = new Vector3(1f, 1f, 1f);
+    
+    void Start()
+    {
+        _orginScale = blackHole.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            blackHole.transform.localScale += new Vector3(0.5f, 0.5f, 0);
+        }
+        
+        if (blackHole.transform.localScale.x > _orginScale.x)
+        {
+            var speed = 1 / decreasingSpeed * Time.deltaTime;
+            blackHole.transform.localScale -= new Vector3(speed, speed, 0);
+        }
+    }
+    
+}
