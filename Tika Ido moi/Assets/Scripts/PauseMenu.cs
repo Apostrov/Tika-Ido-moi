9de2cd938599d5f85b2e11587c6df164b5730f86
Dinmukhametov Salavat <salavatarus@gmail.com>
2019-01-26 20:38:52 +0300

    simple gui

---
+++ Tika Ido moi/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPause = false;
+    public GameObject pauseUI;
+
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPause) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume(){
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPause = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Pause(){
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPause = true;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
+    public void LoadMenu(){
+        Debug.Log("Loading menu...");
+    }
+
+    public void QuitGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
+    public void ReloadGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
